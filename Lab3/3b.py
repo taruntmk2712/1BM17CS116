bb6629fb7e383da84eb8261c03e277f4f98aff27
Tarun M Krishna <43349589+taruntmk2712@users.noreply.github.com>
2019-09-05 11:56:34 +0530

    Create 3b.py

---
+++ Lab3/3b.py
@@ -0,0 +1,20 @@
+import random
+import string
+def func():
+    length = random.randrange(8,15)
+    count = 0
+    pwd = " "
+    while count != length:
+        upper = [random.choice(string.ascii_uppercase)]
+        lower = [random.choice(string.ascii_lowercase)]
+        num = [random.choice(string.digits)]
+        pun = [random.choice(string.punctuation)]
+        p_all = upper + lower + num + pun
+        pwd += random.choice(p_all)
+        count += 1
+        continue
+    if count == length :
+        print("Password:",pwd)
+
+
+func()
