3fa4b8a2aef78fa2b68668a7e169f710f0d76251
Tarun M Krishna <43349589+taruntmk2712@users.noreply.github.com>
2019-09-13 07:24:36 +0530

    Update 3b.py

--- Lab3/3b.py
+++ Lab3/3b.py
@@ -1,23 +1,26 @@
 import random
 import string
-def func():
+def pwd():
+    pwd = []
+    pass1 = " "
+    pwd1 = []
     length = random.randrange(8,15)
     count = 0
-    pwd = " "
-    while count != length:
-        upper = [random.choice(string.ascii_uppercase)]
-        lower = [random.choice(string.ascii_lowercase)]
-        num = [random.choice(string.digits)]
-        pun = [random.choice(string.punctuation)]
-        p_all = upper + lower + num + pun
-        pwd += random.choice(p_all)
-        count += 1
-        continue
-    if count == length :
-        print("Password:",pwd)
-
-
-func()
+    upper = [random.choice(string.ascii_uppercase)]
+    lower = [random.choice(string.ascii_lowercase)]
+    num = [random.choice(string.digits)]
+    symbol = [random.choice(string.punctuation)]
+    p_all = upper + lower + num + symbol
+    if length == 4:
+        pwd = random.sample(p_all,length)
+    else:
+        for i in range(0,length-4):
+            pwd1 += random.sample([random.choice(string.ascii_uppercase)]+[random.choice(string.ascii_lowercase)]+[random.choice(string.digits)]+[random.choice(string.punctuation)],1)
+        pwd = p_all+pwd1
+    for i in pwd:
+        pass1 += i
+    print("Password:",pass1)
+    
 ''' 
 OUTPUT:
 Password:  _`NFGm2rwf%1]5
