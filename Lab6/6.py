b359459991dee796854f2f3699851b7ba477ead7
Tarun M Krishna <43349589+taruntmk2712@users.noreply.github.com>
2019-10-10 12:11:55 +0530

    Update 6.py

--- Lab6/6.py
+++ Lab6/6.py
@@ -1,41 +1,39 @@
 class Brackets:
-    def __init__(s,a):
-        s.a = a 
+    def __init__(s,str1):
+        s.str1 = str1
 
-    def func(s,a):
-        count = 0
-        b = []
-        for i,j in zip(range(0 , n-1) , range(1 ,n) ):
-            if s.a[i] == '{':
-                if s.a[j] == '}':
-                    b.append(1)
-                else:
-                    b.append(0)
-            elif s.a[i] == '(':
-                if s.a[j] == ')':
-                    b.append(1)
-                else:
-                    b.append(0)
-            elif s.a[i] == '[':
-                if s.a[j] == ']':
-                    b.append(1)
-                else:
-                    b.append(0)
-        if all( i == 1 for i in b):
-            return True
-        else:
-            return False
-   
-
-a = []
+    def func(s):
+       open_b = tuple('({[')
+       close_b = tuple(')}]')
+       balance = dict(zip(open_b,close_b))
+       a = []
+       for i in s.str1:
+           if i in open_b:
+               a.append(balance[i])
+           elif i in close_b:
+               if not a or i != a.pop():
+                   return False
+       return True
+              
 str1 = input("Enter Brackets:")
 n = len(str1)
 if n%2 == 0:
-    a = list(str1)
-    b1 = Brackets(a)
-    if b1.func(a) == True:
+    b1 = Brackets(str1)
+    if b1.func() == True:
         print("Valid Pair's")
-    elif b1.func(a) == False:
+    elif b1.func() == False:
         print("Invalid Pair's")
 else:
     print("Bracket did not form pairs")
+
+'''
+OUTPUT:
+Enter Brackets:{}[({{}})]
+Valid Pair's
+
+Enter Brackets:[](]
+Invalid Pair's
+
+Enter Brackets:[](
+Bracket did not form pairs
+'''
